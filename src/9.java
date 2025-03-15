0e0e9306bc5cfd54c713ae4478c53721d3b83301
vttparfumerie <vttparfumerie@gmail.com>
2025-03-15 11:22:11 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,6 @@
+public class SchoolProject {
+    public static void main(String[] args) {
+        System.out.println("Welcome to your school project repository!");
+        // TODO: Add your project code here
+    }
+}
