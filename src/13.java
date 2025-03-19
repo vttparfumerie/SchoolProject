6bb851cf1bfd848d0d897a107111e8a71cebaf1c
vttparfumerie <vttparfumerie@gmail.com>
2025-03-19 21:31:12 +0000

    Add 13.java

---
+++ src/13.java
@@ -0,0 +1,9 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
