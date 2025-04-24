84d71a648bf42c6ccb66726bae9985572bf767ce
vttparfumerie <vttparfumerie@gmail.com>
2025-04-24 15:12:32 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Example Java program with main method and arguments
+        System.out.println("Hello World!");
+    }
+}
