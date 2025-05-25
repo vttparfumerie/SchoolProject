cb3540cb3da0de9565552146cd1377737a548819
vttparfumerie <vttparfumerie@gmail.com>
2025-05-25 04:03:24 +0000

    Add 45.java

---
+++ src/45.java
@@ -0,0 +1,11 @@
+public class SchoolProject {
+    private String name;
+    private int age;
+
+    public SchoolProject(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Add more methods or constructors as needed
+}
